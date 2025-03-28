7895fdc96f1f07ec817ab25ae998ed350f9ec54b
PG <pedro.gustavo@bugaboostudio.com>
2025-03-28 14:47:53 -0300

    1.4.4

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-JogoDosDiagramasSenai-1.4.3";
+const cacheName = "DefaultCompany-JogoDosDiagramasSenai-1.4.4";
 const contentToCache = [
     "Build/SenaiDiagramas.loader.js",
     "Build/SenaiDiagramas.framework.js",
